95ac9e0ac44db6dc5cf2c6ac8d03dd850eea4de5
Paul Schoenfelder <paulschoenfelder@gmail.com>
2012-01-05 14:28:32 -0600

    Added utility tests and more stringex tests.

--- test/test-stringex.js
+++ test/test-stringex.js
@@ -7,8 +7,40 @@ vows.describe('String Extensions').addBatch({
 	'when converting the string "$2 Soda" to a url-friendly format': {
 		topic: function() { return stringex.toUrl('$2 Soda'); },
 
-		'we get two-dollar-soda as a result': function(topic) {
-			assert.equal(topic, 'two-dollar-soda');
+		'we get 2-dollars-soda as a result': function(topic) {
+			assert.equal(topic, '2-dollars-soda');
+		}
+	},
+
+	'when stripping html tags from "<p>Hello!</p>"': {
+		topic: function() { return stringex.stripHtmlTags("<p>Hello!</p>"); },
+
+		'we get "Hello!" as a result': function(topic) {
+			assert.equal(topic, 'Hello!');
+		}
+	},
+
+	'when squeezing the string "yellow moon"': {
+		topic: function() { return stringex.squeeze('yellow moon'); },
+
+		'we get "yelow mon" as a result': function(topic) {
+			assert.equal(topic, 'yelow mon');
+		}
+	},
+
+	'when squeezing the string "  now    is  the", with " " as a parameter': {
+		topic: function() { return stringex.squeeze('  now    is  the', ' '); },
+
+		'we get " now is the" as a result': function(topic) {
+			assert.equal(topic, ' now is the');
+		}
+	},
+
+	'when squeezing the string "putters shoot balls" with "m-z" as a parameter': {
+		topic: function() { return stringex.squeeze('putters shoot balls', 'm-z'); },
+
+		'we get "puters shot balls" as a result': function(topic) {
+			assert.equal(topic, 'puters shot balls');
 		}
 	}
-}).run();
+}).export(module);
