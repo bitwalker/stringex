07d8cc2c21a25fc52b5f0cc2f010e55ba86ccb90
Henri Bergius <henri.bergius@iki.fi>
2012-04-24 18:46:32 +0200

    Add test for Scandic characters

--- test/test-unidecoder.js
+++ test/test-unidecoder.js
@@ -5,6 +5,9 @@ var vows       = require('vows'),
 var encoded = "Today it was -140° outside!";
 var decoded = "Today it was -140deg outside!";
 
+var scandicEncoded = "älä lyö ääliö ööliä läikkyy";
+var scandicDecoded = "ala lyo aalio oolia laikkyy";
+
 vows.describe('Unidecoder').addBatch({
 
   'when decoding "Today it was -140° outside!" from unicode to ASCII': {
@@ -13,6 +16,13 @@ vows.describe('Unidecoder').addBatch({
     'the result should be "Today it was -140deg outside!"': function(topic) {
       assert.equal(topic, decoded);
     },
+  },
+
+  'when decoding "älä lyö ääliö ööliä läikkyy" from unicode to ASCII': {
+    topic: function() { return Unidecoder.decode(scandicEncoded); },
+  'the result should be "ala lyo aalio oolia laikkyy"': function(topic) {
+    assert.equal(topic, scandicDecoded);
+  }
   }
 
 }).export(module);
